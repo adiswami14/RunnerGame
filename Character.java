b6652ec40612bf227cc35d33285c5b181d057088
adiswami14 <adithya1431@gmail.com>
2020-05-22 21:22:49 -0400

    added keyListener to character

--- Character.java
+++ Character.java
@@ -1,4 +1,51 @@
-public class Character extends World
+import java.awt.event.*;
+public class Character extends World implements KeyListener
 {
+    private int x;
+    private int y;
+    private int speed = 2; //rate of ground movement beneath character
+    private int jumpHeight = 30; //height that character jumps when jump method is called
+    private Character character;
+    public Character(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        character = new Character(x, y);
+    }
+    public int getJumpHeight()
+    {
+        return jumpHeight;
+    }
+    public void setJumpHeight(int jumpHeight)
+    {
+        this.jumpHeight = jumpHeight;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public void jump()
+    {
+        
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
 
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        if(e.getKeyCode() == 32) //spacebar
+        {
+            character.jump();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+    }
 }
