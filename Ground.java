e179ea3b6c983967cf4369e392682f6e2ec4ea71
Zubair Lalani <zubairlalani07@gmail.com>
2020-05-22 20:50:46 -0400

    Added basic stucture for Ground class

--- Ground.java
+++ Ground.java
@@ -1,7 +1,27 @@
+import java.awt.Color;
 public class Ground extends World
 {
-    public Ground()
+	int height;
+	boolean pit;
+	Color col;
+    public Ground(int hieght, boolean pit, Color col)
     {
-        
+
     }
+    public int getHeight()
+    {
+		return height;
+	}
+	public boolean getPit(){
+		return pit;
+	}
+	public Color getColor(){
+		return col;
+	}
+	public void setHeight(int height){
+		this.height = height;
+	}
+	public void setColor(Color col){
+		this.col = col;
+	}
 }
