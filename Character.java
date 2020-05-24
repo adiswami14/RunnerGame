e50a1f4b2bd4ae3e6f8e79b045e75725a692f609
adiswami14 <adithya1431@gmail.com>
2020-05-24 09:57:24 -0400

    Jumping works

--- Character.java
+++ Character.java
@@ -1,30 +1,22 @@
 import java.awt.event.*;
 import java.io.*;
 import java.io.Console;
-public class Character extends World
-{
-    private int speed = 2; //rate of ground movement beneath character
-    private int jumpHeight = 30; //height that character jumps when jump method is called
-    public Character(int x, int y)
-    {
+import javax.swing.Timer;
+
+public class Character extends World {
+    private int speed = 2; // rate of ground movement beneath character
+    public boolean isJumping = false;
+    public Character(int x, int y) {
         super(x, y);
-    }
-    public int getJumpHeight()
-    {
-        return jumpHeight;
-    }
-    public void setJumpHeight(int jumpHeight)
-    {
-        this.jumpHeight = jumpHeight;
     }
     public int getSpeed() {
         return speed;
     }
+
     public void setSpeed(int speed) {
         this.speed = speed;
     }
-    public void jump()
-    {
-        setY(getY()-jumpHeight);
+    public void jump() {
+        isJumping = true;        
     }
 }
--- Game.java
+++ Game.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Game extends JPanel implements KeyListener
-{
+
+public class Game extends JPanel implements KeyListener {
     JFrame frame;
     World world;
     Character player;
@@ -11,44 +11,50 @@ public class Game extends JPanel implements KeyListener
     int playerX = 30, playerY = 0;
     int frameHeight = 600;
     int frameWidth = 1000;
-    public Game()
-    {
+
+    public Game() {
         instantiateFrame();
     }
-    public void paintComponent(Graphics g)
-    {
-		super.paintComponent(g);
-		g.setColor(Color.BLACK);
-		g.fillRect(0,0, frame.getWidth(), frame.getHeight());	//Created Rectangle for frame
 
-		g.setColor(ground.getColor());
-		int count =0;
-		for(int x=0; x<frame.getWidth(); x++){
-			if(x%100 == 0){ //evenly divides width into 10 rectangles
-				System.out.println("COUNT: "+count);
-				if(ground.getPitPosition().get(count) == false){
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, frame.getWidth(), frame.getHeight()); // Created Rectangle for frame
+        g.setColor(ground.getColor());
+        int count = 0;
+        for (int x = 0; x < frame.getWidth(); x++) {
+            if (x % 100 == 0) { // evenly divides width into 10 rectangles
+                System.out.println("COUNT: " + count);
+                if (ground.getPitPosition().get(count) == false) {
 
-					g.fillRect(x, frameHeight-ground.getHeight(), frameWidth, ground.getHeight());
-				}
-				count++;
-			}
-		}
+                    g.fillRect(x, frameHeight - ground.getHeight(), frameWidth, ground.getHeight());
+                }
+                count++;
+            }
+        }
 
-       // g.fillRect(0, frameHeight-ground.getHeight(), frameWidth, ground.getHeight()); //so this goes from bottom to top instead of top to bottom
+        // g.fillRect(0, frameHeight-ground.getHeight(), frameWidth,
+        // ground.getHeight()); //so this goes from bottom to top instead of top to
+        // bottom
 
         g.setColor(Color.RED);
-        g.setFont(new Font("Times New Roman", Font.BOLD,40));
-        g.drawString("Player X: "+player.getX(), 500, 200);
-        g.drawString("Player Y: "+player.getY(), 500, 250);
-        g.fillRect(player.getX(), player.getY()-40, 40, 40);
+        g.setFont(new Font("Times New Roman", Font.BOLD, 40));
+        g.drawString("Player X: " + player.getX(), 500, 200);
+        g.drawString("Player Y: " + player.getY(), 500, 250);
+        g.fillRect(player.getX(), player.getY() - 40, 40, 40);
+        if(player.isJumping)
+        {
+            player.setY(player.getY()-1);
+        }
+        else if(!player.isJumping && player.getY()<frameHeight - ground.getHeight())
+            player.setY(player.getY()+1);
+        repaint();
+    }
 
-
-	}
-    public void instantiateFrame()
-    {
+    public void instantiateFrame() {
         frame = new JFrame();
-        world = new World(0,0);
-        player = new Character(30, frameHeight-ground.getHeight());
+        world = new World(0, 0);
+        player = new Character(30, frameHeight - ground.getHeight());
         this.addKeyListener(this);
         frame.add(this);
         frame.setSize(frameWidth, frameHeight);
@@ -56,6 +62,7 @@ public class Game extends JPanel implements KeyListener
         this.requestFocusInWindow();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
     @Override
     public void keyTyped(KeyEvent e) {
         // TODO Auto-generated method stub
@@ -65,25 +72,40 @@ public class Game extends JPanel implements KeyListener
     @Override
     public void keyPressed(KeyEvent e) {
         // TODO Auto-generated method stub
-        if(e.getKeyCode() == 32) //spacebar
-		{
+        if (e.getKeyCode() == 32) // spacebar
+        {
             player.jump();
-            System.out.println("jump");
+            new Thread(new thread()).start();
+        }
+        if (e.getKeyCode() == 39) // right
+        {
+            ground.setPitPosition(ground.getPitPositionSize() - 1, true);
+            System.out.println("IN RIGHT");
+            System.out.println(ground.getPitPosition());
             repaint();
         }
-		if(e.getKeyCode() == 39) //right
-		{
-			ground.setPitPosition(ground.getPitPositionSize()-1, true);
-			System.out.println("IN RIGHT");
-			System.out.println(ground.getPitPosition());
-			repaint();
-		}
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         // TODO Auto-generated method stub
 
+    }
+
+    public class thread implements Runnable {
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            try {
+                Thread.sleep(200);
+                player.isJumping = false;
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                new Thread(this).start();
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }  
     }
     public static void main(String[] args)
     {
