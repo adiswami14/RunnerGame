a7d3750b88b448e33c36a6c8260015ed9289429b
Zubair Lalani <zubairlalani07@gmail.com>
2020-06-16 16:07:52 -0400

    drew oval for powerup

--- Powerup.java
+++ Powerup.java
@@ -1,7 +1,28 @@
-public class Powerup extends World
+import java.awt.event.*;
+import java.io.*;
+import java.io.Console;
+import javax.swing.Timer;
+import java.awt.*;
+public class Powerup extends Block
 {
-    public Powerup(double x, double y)
+	private int starInterval = 5;
+    public Powerup(double x, double y, boolean isPit, boolean hasObstacle)
     {
-        super(x, y);
+        super(x, y, isPit, hasObstacle);
     }
+    public void initStart(){
+
+	}
+	public int getStarInterval(){
+		return starInterval;
+	}
+	public void setStarInterval(int starInterval){
+		this.starInterval = starInterval;
+	}
+	public void draw(Graphics g, Ground ground)
+    {
+		g.setColor(Color.YELLOW);
+		g.fillOval((int)getX(), (int)getY(), 50, 50);		//random test drawing of circle
+	}
+
 }
